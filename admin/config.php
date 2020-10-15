3ea3bfc16f0ee968f974e4d3b93ab24e4c13eb81
developermwangimuthui <intelligentsoftwaresdev@gmail.com>
2020-10-15 10:19:59 +0300

    first commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/Realestate/admin/');
+define('HTTP_CATALOG', 'http://localhost/Realestate/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/Realestate/admin/');
+define('HTTPS_CATALOG', 'http://localhost/Realestate/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/RealEstate/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/RealEstate/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/RealEstate/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/RealEstate/admin/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/RealEstate/admin/view/template/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/RealEstate/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/RealEstate/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/RealEstate/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/RealEstate/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/RealEstate/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/RealEstate/system/storage/upload/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/RealEstate/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'realestate');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/Realestate/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/Realestate/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/RealEstate/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/RealEstate/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/RealEstate/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/RealEstate/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/RealEstate/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/RealEstate/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/RealEstate/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/RealEstate/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/RealEstate/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/RealEstate/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/RealEstate/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'realestate');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
