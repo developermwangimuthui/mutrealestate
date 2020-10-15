04dd93212b4f3048d460671808a6a44582124ee5
developermwangimuthui <intelligentsoftwaresdev@gmail.com>
2020-10-15 13:48:06 +0300

    online config

--- admin/config.php
+++ admin/config.php
@@ -1,31 +1,67 @@
 <?php
-// HTTP
-define('HTTP_SERVER', 'http://localhost/Realestate/admin/');
-define('HTTP_CATALOG', 'http://localhost/Realestate/');
+// // HTTP
+// define('HTTP_SERVER', 'http://localhost/Realestate/admin/');
+// define('HTTP_CATALOG', 'http://localhost/Realestate/');
+
+// // HTTPS
+// define('HTTPS_SERVER', 'http://localhost/Realestate/admin/');
+// define('HTTPS_CATALOG', 'http://localhost/Realestate/');
+
+// // DIR
+// define('DIR_APPLICATION', 'C:/xampp/htdocs/RealEstate/admin/');
+// define('DIR_SYSTEM', 'C:/xampp/htdocs/RealEstate/system/');
+// define('DIR_IMAGE', 'C:/xampp/htdocs/RealEstate/image/');
+// define('DIR_LANGUAGE', 'C:/xampp/htdocs/RealEstate/admin/language/');
+// define('DIR_TEMPLATE', 'C:/xampp/htdocs/RealEstate/admin/view/template/');
+// define('DIR_CONFIG', 'C:/xampp/htdocs/RealEstate/system/config/');
+// define('DIR_CACHE', 'C:/xampp/htdocs/RealEstate/system/storage/cache/');
+// define('DIR_DOWNLOAD', 'C:/xampp/htdocs/RealEstate/system/storage/download/');
+// define('DIR_LOGS', 'C:/xampp/htdocs/RealEstate/system/storage/logs/');
+// define('DIR_MODIFICATION', 'C:/xampp/htdocs/RealEstate/system/storage/modification/');
+// define('DIR_UPLOAD', 'C:/xampp/htdocs/RealEstate/system/storage/upload/');
+// define('DIR_CATALOG', 'C:/xampp/htdocs/RealEstate/catalog/');
+
+// // DB
+// define('DB_DRIVER', 'mysqli');
+// define('DB_HOSTNAME', 'localhost');
+// define('DB_USERNAME', 'root');
+// define('DB_PASSWORD', '');
+// define('DB_DATABASE', 'realestate');
+// define('DB_PORT', '3306');
+// define('DB_PREFIX', 'oc_');
+
+
+
+
+// online
+// <?php
+// // HTTP
+define('HTTP_SERVER', 'http://rent.osofyahtech.com/admin/');
+define('HTTP_CATALOG', 'http://rent.osofyahtech.com/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost/Realestate/admin/');
-define('HTTPS_CATALOG', 'http://localhost/Realestate/');
+define('HTTPS_SERVER', 'http://rent.osofyahtech.com/admin/');
+define('HTTPS_CATALOG', 'http://rent.osofyahtech.com/');
 
 // DIR
-define('DIR_APPLICATION', 'C:/xampp/htdocs/RealEstate/admin/');
-define('DIR_SYSTEM', 'C:/xampp/htdocs/RealEstate/system/');
-define('DIR_IMAGE', 'C:/xampp/htdocs/RealEstate/image/');
-define('DIR_LANGUAGE', 'C:/xampp/htdocs/RealEstate/admin/language/');
-define('DIR_TEMPLATE', 'C:/xampp/htdocs/RealEstate/admin/view/template/');
-define('DIR_CONFIG', 'C:/xampp/htdocs/RealEstate/system/config/');
-define('DIR_CACHE', 'C:/xampp/htdocs/RealEstate/system/storage/cache/');
-define('DIR_DOWNLOAD', 'C:/xampp/htdocs/RealEstate/system/storage/download/');
-define('DIR_LOGS', 'C:/xampp/htdocs/RealEstate/system/storage/logs/');
-define('DIR_MODIFICATION', 'C:/xampp/htdocs/RealEstate/system/storage/modification/');
-define('DIR_UPLOAD', 'C:/xampp/htdocs/RealEstate/system/storage/upload/');
-define('DIR_CATALOG', 'C:/xampp/htdocs/RealEstate/catalog/');
+define('DIR_APPLICATION', '/home/pickiepr/rent.osofyahtech.com/admin/');
+define('DIR_SYSTEM', '/home/pickiepr/rent.osofyahtech.com/system/');
+define('DIR_IMAGE', '/home/pickiepr/rent.osofyahtech.com/image/');
+define('DIR_LANGUAGE', '/home/pickiepr/rent.osofyahtech.com/admin/language/');
+define('DIR_TEMPLATE', '/home/pickiepr/rent.osofyahtech.com/admin/view/template/');
+define('DIR_CONFIG', '/home/pickiepr/rent.osofyahtech.com/system/config/');
+define('DIR_CACHE', '/home/pickiepr/rent.osofyahtech.com/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/pickiepr/rent.osofyahtech.com/system/storage/download/');
+define('DIR_LOGS', '/home/pickiepr/rent.osofyahtech.com/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/pickiepr/rent.osofyahtech.com/system/storage/modification/');
+define('DIR_UPLOAD', '/home/pickiepr/rent.osofyahtech.com/system/storage/upload/');
+define('DIR_CATALOG', '/home/pickiepr/rent.osofyahtech.com/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'realestate');
+define('DB_USERNAME', 'pickiepr_rent');
+define('DB_PASSWORD', 'uC91=mfyauQ&');
+define('DB_DATABASE', 'pickiepr_rent');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
--- config.php
+++ config.php
@@ -1,28 +1,60 @@
+<!-- offline -->
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost/Realestate/');
+// define('HTTP_SERVER', 'http://localhost/Realestate/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost/Realestate/');
+// define('HTTPS_SERVER', 'http://localhost/Realestate/');
 
 // DIR
-define('DIR_APPLICATION', 'C:/xampp/htdocs/RealEstate/catalog/');
-define('DIR_SYSTEM', 'C:/xampp/htdocs/RealEstate/system/');
-define('DIR_IMAGE', 'C:/xampp/htdocs/RealEstate/image/');
-define('DIR_LANGUAGE', 'C:/xampp/htdocs/RealEstate/catalog/language/');
-define('DIR_TEMPLATE', 'C:/xampp/htdocs/RealEstate/catalog/view/theme/');
-define('DIR_CONFIG', 'C:/xampp/htdocs/RealEstate/system/config/');
-define('DIR_CACHE', 'C:/xampp/htdocs/RealEstate/system/storage/cache/');
-define('DIR_DOWNLOAD', 'C:/xampp/htdocs/RealEstate/system/storage/download/');
-define('DIR_LOGS', 'C:/xampp/htdocs/RealEstate/system/storage/logs/');
-define('DIR_MODIFICATION', 'C:/xampp/htdocs/RealEstate/system/storage/modification/');
-define('DIR_UPLOAD', 'C:/xampp/htdocs/RealEstate/system/storage/upload/');
+// define('DIR_APPLICATION', 'C:/xampp/htdocs/RealEstate/catalog/');
+// define('DIR_SYSTEM', 'C:/xampp/htdocs/RealEstate/system/');
+// define('DIR_IMAGE', 'C:/xampp/htdocs/RealEstate/image/');
+// define('DIR_LANGUAGE', 'C:/xampp/htdocs/RealEstate/catalog/language/');
+// define('DIR_TEMPLATE', 'C:/xampp/htdocs/RealEstate/catalog/view/theme/');
+// define('DIR_CONFIG', 'C:/xampp/htdocs/RealEstate/system/config/');
+// define('DIR_CACHE', 'C:/xampp/htdocs/RealEstate/system/storage/cache/');
+// define('DIR_DOWNLOAD', 'C:/xampp/htdocs/RealEstate/system/storage/download/');
+// define('DIR_LOGS', 'C:/xampp/htdocs/RealEstate/system/storage/logs/');
+// define('DIR_MODIFICATION', 'C:/xampp/htdocs/RealEstate/system/storage/modification/');
+// define('DIR_UPLOAD', 'C:/xampp/htdocs/RealEstate/system/storage/upload/');
+
+// // DB
+// define('DB_DRIVER', 'mysqli');
+// define('DB_HOSTNAME', 'localhost');
+// define('DB_USERNAME', 'root');
+// define('DB_PASSWORD', '');
+// define('DB_DATABASE', 'realestate');
+// define('DB_PORT', '3306');
+// // define('DB_PREFIX', 'oc_');
+
+
+
+// online
+// HTTP
+define('HTTP_SERVER', 'http://rent.osofyahtech.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://rent.osofyahtech.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/pickiepr/rent.osofyahtech.com/catalog/');
+define('DIR_SYSTEM', '/home/pickiepr/rent.osofyahtech.com/system/');
+define('DIR_IMAGE', '/home/pickiepr/rent.osofyahtech.com/image/');
+define('DIR_LANGUAGE', '/home/pickiepr/rent.osofyahtech.com/catalog/language/');
+define('DIR_TEMPLATE', '/home/pickiepr/rent.osofyahtech.com/catalog/view/theme/');
+define('DIR_CONFIG', '/home/pickiepr/rent.osofyahtech.com/system/config/');
+define('DIR_CACHE', '/home/pickiepr/rent.osofyahtech.com/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/pickiepr/rent.osofyahtech.com/system/storage/download/');
+define('DIR_LOGS', '/home/pickiepr/rent.osofyahtech.com/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/pickiepr/rent.osofyahtech.com/system/storage/modification/');
+define('DIR_UPLOAD', '/home/pickiepr/rent.osofyahtech.com/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'realestate');
+define('DB_USERNAME', 'pickiepr_rent');
+define('DB_PASSWORD', 'uC91=mfyauQ&');
+define('DB_DATABASE', 'pickiepr_rent');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
